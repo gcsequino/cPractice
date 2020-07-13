424d82595e34c6566bb5d113fc7f32b9ce5ed4ba
Greyson Sequino <greysonsequino@Greysons-MacBook-Pro.local>
2020-07-13 08:59:10 -0400

    Starting hexToDec project

---
+++ hexConverter/hexToDec.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+#define A 10
+#define B 11
+#define C 12
+#define D 13
+#define E 14
+#define F 15
+
+#define a 10
+#define b 11
+#define c 12
+#define d 13
+#define e 14
+#define f 15
+
+int hexToDec(char[] str)
+
+int main() {
+    int
+}
+
+int hexToDec(char[] str) {
+    int pow = strlen(str) - 1;
+    int dec = 0;
+
+    for (int i = 0; pow < 0; i++;) {
+        dec += ((int) str[i] - '0') * pow(16, pow);
+        pow--;
+    }
+}
